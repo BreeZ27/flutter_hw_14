05d40c683443bb4fd1adcdcee3c5c6a52ddda8b0
BreeZ27 <khavlaan@gmail.com>
2022-05-26 22:42:56 +0300

    Update main.dart

--- flutter_hw_14/lib/main.dart
+++ flutter_hw_14/lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+ThemeData currentTheme = ThemeData(primarySwatch: Colors.blueGrey);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,16 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: currentTheme,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -29,39 +31,108 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late final PageStyleCubit _pageStyleCubit;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final Map<MaterialColor, String> colorsMap = {
+    Colors.blueGrey: 'Серый',
+    Colors.blue: 'Синий',
+    Colors.green: 'Зелёный',
+    Colors.deepPurple: 'Фиолетовый',
+  };
+  Color? _currentColor;
+
+  @override
+  void initState() {
+    // _currentColor = colorsMap.keys[0];
+
+    _pageStyleCubit = PageStyleCubit();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageStyleCubit.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return StreamBuilder(
+      stream: _pageStyleCubit.themeColorState,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          // _currentColor = snapshot.data;
+          currentTheme = snapshot.data;
+        }
+        return StyleAdjustmentWidget(
+          themeData: ThemeData(
+              primaryColor:
+                  _currentColor != null ? _currentColor! : Colors.red),
+          child: Builder(
+            builder: (BuildContext innerContext) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(widget.title),
+                ),
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ...colorsMap.keys.map(
+                        (e) => ElevatedButton(
+                          onPressed: () {
+                            if (e == _currentColor) {
+                              _pageStyleCubit
+                                  .themeColorEventHandler(Colors.red);
+                            } else {
+                              _pageStyleCubit.themeColorEventHandler(e);
+                            }
+                          },
+                          child: Text('${colorsMap[e]}'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
+  }
+}
+
+class StyleAdjustmentWidget extends InheritedWidget {
+  StyleAdjustmentWidget(
+      {Key? key, required this.child, required this.themeData})
+      : super(key: key, child: child);
+
+  final Widget child;
+  final ThemeData themeData;
+  // ThemeData themeData = ThemeData(primaryColor: color);
+
+  static StyleAdjustmentWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<StyleAdjustmentWidget>();
+  }
+
+  @override
+  bool updateShouldNotify(StyleAdjustmentWidget oldWidget) {
+    return true;
+  }
+}
+
+class PageStyleCubit {
+  final _themeColorStateController = StreamController<ThemeData>();
+
+  Stream<ThemeData> get themeColorState => _themeColorStateController.stream;
+
+  void themeColorEventHandler(MaterialColor color) {
+    _themeColorStateController.add(ThemeData(primarySwatch: color));
+    print('[PageStyleCubit themeColorEventHandler($color)]');
+  }
+
+  void dispose() {
+    _themeColorStateController.close();
   }
 }
